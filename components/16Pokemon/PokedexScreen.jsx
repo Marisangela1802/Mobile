b85863f1cdea7950861e31a08f8fee4090fad652
Marisangela <marisangelalima110@gmail.com>
2023-10-04 14:08:00 -0300

    Pokedex

--- components/16Pokemon/PokedexScreen.jsx
+++ components/16Pokemon/PokedexScreen.jsx
@@ -1,7 +1,9 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, FlatList, ScrollView, Image } from "react-native";
 import { useState, useEffect } from "react";
 
-const PokedexScreen = ({navigation}) => {
+const PokedexScreen = () => {
+
+    const [pokemons, setPokemons] = useState([])
 
     useEffect(
         () => {
@@ -12,25 +14,57 @@ const PokedexScreen = ({navigation}) => {
 
     const fetchPokemons = () => {
         fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
-        .then(response => response.json())
-        .then(
-            json => {
-                const vetorFormatado = json.results.map(
-                    ({name, url}) => {
-                        return {id:url.split("/")[6], name, url}
+            .then(response => response.json())
+            .then(
+                (json) => {
+                    const vetorFormatado = json.results.map(
+                        ({ name, url }) => {
+                            return { id: url.split("/")[6], name, url }
+                        }
+                    )
+                    // console.log(vetorFormatado)
+                    setPokemons(vetorFormatado)
+                })
+            .catch(error => console.log(error))
+    }
+
+    const renderPokemons = () => {
+        return (
+            <ScrollView>
+                <View
+                    style={{
+                        flex:1,
+                        flexDirection:"row",
+                        justufyContent:"space-evenly",
+                        flexWrap:"wrap"
+                    }}
+                >
+                    {
+                        //map retorna um vetor
+                        pokemons.map(
+                            (pokemonObj) => {
+                                return (
+                                    <View>
+                                        <Text>{pokemonObj.name.toUpperCase()}</Text>
+                                        <Image
+                                            source={{uri:`url`}}
+                                            style={{width:60, height:60}}
+                                        />
+                                    </View>
+                                )
+                            }
+                        )
                     }
-                )
-            })
-        .catch(error=>console.log(error))
+                </View>
+            </ScrollView>
+        )
     }
 
     return (
         <View>
             <Text>Pokedex</Text>
-            <Button
-                title="ABRIR MODAL"
-                onPress={()=>navigation.navigate("PokemonModal")}
-            />
+    
+            {renderPokemons()}
         </View>
     )
 }
